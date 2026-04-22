fd0057f414d35c13ec57c5b5fd2350b713a94372
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2026-04-21 20:34:05 -0500

    Estrcutura inicial

---
+++ src/test/java/LAMENTACIONES_SUPERVISION/com/lamentaciones/supervision/ApplicationTests.java
@@ -0,0 +1,13 @@
+package LAMENTACIONES_SUPERVISION.com.lamentaciones.supervision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
